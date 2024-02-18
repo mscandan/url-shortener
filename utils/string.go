db96028d91112e68ff8d9e1961424a4fcba1dd08
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2024-02-18 22:37:04 +0300

    feat: add phase1 redirect and shortened url

---
+++ utils/string.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateRandomString(length int) string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+
+	return string(b)
+}
